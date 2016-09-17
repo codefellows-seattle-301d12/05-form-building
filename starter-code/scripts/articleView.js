9e3d783875fbc87d694072f0af5265c27be31999
Rae Helsel <rahelsel@me.com>
2016-09-16 17:27:37 -0700

    workshop pair programming

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -65,6 +65,37 @@ articleView.setTeasers = function() {
   });
 };
 
+articleView.initNewArticlePage = function() {
+  $('#export-field').hide();
+  $('#article-json').on('focus', function() {
+    $(this).select();
+  });
+  $('#new-form').on('change', articleView.create);
+};
+
+articleView.create = function() {
+  $('#article-preview').empty().fadeIn();
+
+  var formArticle = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    category: $('article-category').val(),
+    authorUrl: $('article-author-url').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? new Date() : null
+  });
+
+  $('#article-preview').append(formArticle.toHtml('#article-template'));
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+  $('#export-field').show();
+  $('#article-json').val(',' + JSON.stringify(formArticle));
+};
+
+
+
+articleView.initNewArticlePage();
 articleView.render();
 articleView.handleCategoryFilter();
 articleView.handleAuthorFilter();
